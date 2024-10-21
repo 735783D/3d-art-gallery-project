c80346107508e9452b0ba3f1116bcaa0297c13bf
735783D <88239246+735783D@users.noreply.github.com>
2024-10-21 16:56:53 -0500

    Changed another thing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3d-art-gallery-threejs/",
+  base: "/3d-art-gallery-project/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 })
