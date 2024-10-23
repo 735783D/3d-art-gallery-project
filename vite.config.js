2059faa8e683c086b319bb424cbcc8a4dee1fe6b
735783D <88239246+735783D@users.noreply.github.com>
2024-10-23 15:56:25 -0500

    Fixed it!!

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/3d-art-gallery-project/",
+  build: {
+    assetsInlineLimit: 0 // Force all assets to be served as separate files
+  },
   plugins: [react()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.glb'],
 })
